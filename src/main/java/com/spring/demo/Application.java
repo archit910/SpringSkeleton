167e2f8724be9757192c627f42722ab4982e6546
Archit Chauhan <archit.chauha@tothenew.com>
2018-03-20 00:01:43 +0530

    Starting AOP

--- src/main/java/com/spring/demo/Application.java
+++ src/main/java/com/spring/demo/Application.java
@@ -7,12 +7,6 @@ public class Application {
     public static void main(String[] args) {
 
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("config.xml");
-//
-//        Database database = applicationContext.getBean("databaseBean" , Database.class);
-//        System.out.println(database.getPort() + " " + database.getName());
 
-        Restraunt restraunt = applicationContext.getBean("restraunt" , Restraunt.class);
-        restraunt.getCoffee().prepareHotDrink();
-        restraunt.getTea().prepareHotDrink();
     }
 }
--- src/main/java/com/spring/demo/HotDrink.java
+++
@@ -1,5 +0,0 @@
-package com.spring.demo;
-
-interface HotDrink {
-    abstract void prepareHotDrink() ;
-}
