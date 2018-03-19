cd6d2fd1ee418f769143b23f4e353821f0a96fba
Archit Chauhan <archit.chauha@tothenew.com>
2018-03-20 00:06:05 +0530

    Introduced Java Configuration File

--- src/main/java/com/spring/demo/Application.java
+++ src/main/java/com/spring/demo/Application.java
@@ -1,13 +1,15 @@
 package com.spring.demo;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Application {
     public static void main(String[] args) {
 
-        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("config.xml");
-//
+//        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("config.xml");
+        ApplicationContext applicationContext = new
+                AnnotationConfigApplicationContext(Config.class);
 //        Database database = applicationContext.getBean("databaseBean" , Database.class);
 //        System.out.println(database.getPort() + " " + database.getName());
 
